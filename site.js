a4baf945768968744bd58107ce30ef190a40a42e
BradEstacio <bradestacio@gmail.com>
2022-02-21 14:04:52 -0600

    Add setAttribute() for query selectors

--- site.js
+++ site.js
@@ -2,3 +2,6 @@
 
 var shippingIsBillingFieldset = document.querySelector('fieldset[name="check"]');
 var shippingIsBillingCheckbox = document.querySelector('#show');
+
+shippingIsBillingFieldset.setAttribute('disabled', 'disabled');
+shippingIsBillingFieldset.setAttribute('aria-hidden', 'true');
