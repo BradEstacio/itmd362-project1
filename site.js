612cdc9774d9cb9e32ebf07495f327b48e9ed6c2
BradEstacio <bradestacio@gmail.com>
2022-03-15 00:23:25 -0500

    Move site.js file

--- site.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var shippingIsBillingFieldset = document.querySelector('fieldset[name="check"]');
-var shippingIsBillingCheckbox = document.querySelector('#show');
-
-shippingIsBillingFieldset.setAttribute('disabled', 'disabled');
-shippingIsBillingFieldset.setAttribute('aria-hidden', 'true');
